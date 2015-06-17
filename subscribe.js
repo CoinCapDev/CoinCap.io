071fe1f142af12d1680ffd87c15e462b06e2c3ff
CoinCapDev <matt@shapeshift.io>
2015-06-17 12:45:11 -0600

    Sample socket.js

---
+++ subscribe.js
@@ -0,0 +1,13 @@
+<script src="yourlibpath/socket.io.js"></script>
+
+<script type="text/javascript">
+    var socket = io.connect('http://socket.coincap.io');
+
+    socket.on('trade', function (tradeMsg) {
+        console.log(tradeMsg);
+    })
+
+    socket.on('global', function (globalMsg) {
+        console.log(globalMsg);
+    })
+</script>
